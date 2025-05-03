9d17179e491bcde36a8af921cb00c1f0fba3fe26
radovanne <nradovanovic97@gmail.com>
2025-05-03 13:32:49 +0200

    feat(api): Breaking Change -> function renaming

--- lua/clay.lua
+++ lua/clay.lua
@@ -17,7 +17,7 @@ end
 -- Starts clay session
 -- Requires clay automatically into connected REPL without a need to
 -- put [scicloj.clay.v2.api :as clay] in :require
-M.start = function()
+M.ClayStart = function()
   local code = string.format("(require '[scicloj.clay.v2.api :as clay])")
   conj_eval("clay-start", code)
 end
@@ -25,7 +25,7 @@ end
 -- Starts, evaluates and renders html page in browser.
 -- Requires clay automatically into connected REPL without a need to
 -- put [scicloj.clay.v2.api :as clay] in :require + evaluates and renders html page in the browser.
-M.start_and_render = function()
+M.ClayStartAndRender = function()
   local source_path = vim.fn.expand("%:p")
   local code =
     string.format('(do (require \'[scicloj.clay.v2.api :as clay]) (clay/make! {:source-path "%s"}))', source_path)
@@ -33,7 +33,7 @@ M.start_and_render = function()
 end
 
 -- Evaluates and renders current form in the browser.
-M.eval_form = function()
+M.ClayEvalForm = function()
   local form_content = extract.form({ root = true }).content
   local source_path = vim.fn.expand("%:p")
   local code =
@@ -43,7 +43,7 @@ M.eval_form = function()
 end
 
 -- Evaluates and renders current namespace in browser.
-M.eval_ns = function()
+M.ClayEvalNamespace = function()
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api/make! {:source-path "%s"})', source_path)
   conj_eval("clay-eval-ns", code)
@@ -51,50 +51,50 @@ end
 
 -- Evaluates current namespace and prints as hiccup.
 -- Doesn't render page in the browser.
-M.eval_ns_to_hiccup = function()
+M.ClayEvalNamespaceToHiccup = function()
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api/make-hiccup {:source-path "%s"})', source_path)
   conj_eval("clay-eval-ns-to-hiccup", code)
 end
 
-M.make_ns_html = function()
+M.ClayMakeNamespaceHtml = function()
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api.snippets/make-ns-html! "%s" {:ide :neovim})', source_path)
   conj_eval("make-ns-html!", code)
 end
 
-M.make_ns_quarto_html = function()
+M.ClayMakeNamespaceQuartoHtml = function()
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api.snippets/make-ns-quarto-html! "%s" {:ide :neovim})', source_path)
   conj_eval("make-ns-quarto-html!", code)
 end
 
-M.make_ns_quarto_revealjs = function()
+M.ClayMakeNamespaceQuartoRevealjs = function()
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api.snippets/make-ns-quarto-revealjs! "%s" {:ide :neovim})', source_path)
   conj_eval("make-ns-quarto-revealjs!", code)
 end
 
-M.make_form_html = function()
+M.ClayMakeFormHtml = function()
   local form_content = extract.form({ root = true }).content
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api.snippets/make-form-html! "%s" "%s" {:ide :neovim})', form_content, source_path)
   conj_eval("make-form-html!", code)
 end
 
-M.make_form_quarto_html = function()
+M.MakeFormQuartoHtml = function()
   local form_content = extract.form({ root = true }).content
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api.snippets/make-form-quarto-html! "%s" "%s" {:ide :neovim})', form_content, source_path)
   conj_eval("make-form-quarto-html!", code)
 end
 
-M.browse = function()
+M.ClayBrowse = function()
   local code = string.format('(scicloj.clay.v2.api.snippets/browse!)')
   conj_eval("browse!", code)
 end
 
-M.watch = function()
+M.ClayWatch = function()
   local code = string.format('(scicloj.clay.v2.api.snippets/watch! {:ide :neovim})')
   conj_eval("watch!", code)
 end
