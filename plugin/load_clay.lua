49b140194ca86bfb1bd9e7edbdc52f9442accc59
radovanne <nradovanovic97@gmail.com>
2025-05-06 22:57:00 +0200

    Add more user commands

--- plugin/load_clay.lua
+++ plugin/load_clay.lua
@@ -9,3 +9,23 @@ end, {})
 vim.api.nvim_create_user_command("ClayStart", function()
 require("clay").ClayStart()
 end, {})
+
+vim.api.nvim_create_user_command("MakeFormQuartoHtml", function()
+require("clay").MakeFormQuartoHtml()
+end, {})
+
+vim.api.nvim_create_user_command("ClayMakeFormHtml", function()
+require("clay").ClayMakeFormHtml()
+end, {})
+
+vim.api.nvim_create_user_command("ClayMakeNamespaceQuartoRevealjs", function()
+require("clay").ClayMakeNamespaceQuartoRevealjs()
+end, {})
+
+vim.api.nvim_create_user_command("ClayMakeNamespaceQuartoHtml", function()
+require("clay").ClayMakeNamespaceQuartoHtml()
+end, {})
+
+vim.api.nvim_create_user_command("ClayMakeNamespaceHtml", function()
+require("clay").ClayMakeNamespaceHtml()
+end, {})
