2308c9b3bfda29f4d8653ff954b1368d6ccd3a7e
Nedeljko Radovanovic <67645446+radovanne@users.noreply.github.com>
2025-05-18 23:12:06 +0200

    V2 snippets integration (#3)
    * feat(clay.snippets): Add start! browse! and make-html!
    
    * fix(snippets): Fix api call
    
    * refactor(api): rename api
    
    * fix(api): remove quote from api call param
    
    * feat(api): Fix all api calls
    
    * test(api): Test render namespace function
    
    * Remove quotes from api call param
    
    * refactor(api): Add api calls
    
    Add all api calls and refactor user commands

--- lua/clay.lua
+++ lua/clay.lua
@@ -10,92 +10,104 @@ local function conj_eval(origin, code)
   })
 end
 
-M.setup = function()
-  -- nothing yet
-end
-
--- Starts clay session
--- Requires clay automatically into connected REPL without a need to
--- put [scicloj.clay.v2.api :as clay] in :require
-M.ClayStart = function()
-  local code = string.format("(require '[scicloj.clay.v2.api :as clay])")
-  conj_eval("clay-start", code)
-end
-
--- Starts, evaluates and renders html page in browser.
--- Requires clay automatically into connected REPL without a need to
--- put [scicloj.clay.v2.api :as clay] in :require + evaluates and renders html page in the browser.
-M.ClayStartAndRender = function()
-  local source_path = vim.fn.expand("%:p")
-  local code =
-    string.format('(do (require \'[scicloj.clay.v2.api :as clay]) (clay/make! {:source-path "%s"}))', source_path)
-  conj_eval("clay-start-and-render", code)
-end
-
--- Evaluates and renders current form in the browser.
-M.ClayEvalForm = function()
-  local form_content = extract.form({ root = true }).content
-  local source_path = vim.fn.expand("%:p")
-  local code =
-    string.format('(scicloj.clay.v2.api/make! {:source-path "%s" :single-form %s})', source_path, form_content)
-
-  conj_eval("clay-eval-form", code)
-end
-
--- Evaluates and renders current namespace in browser.
-M.ClayEvalNamespace = function()
-  local source_path = vim.fn.expand("%:p")
-  local code = string.format('(scicloj.clay.v2.api/make! {:source-path "%s"})', source_path)
-  conj_eval("clay-eval-ns", code)
-end
-
 -- Evaluates current namespace and prints as hiccup.
 -- Doesn't render page in the browser.
-M.ClayEvalNamespaceToHiccup = function()
+M.ClayPrintNamespaceToHiccup = function()
   local source_path = vim.fn.expand("%:p")
   local code = string.format('(scicloj.clay.v2.api/make-hiccup {:source-path "%s"})', source_path)
   conj_eval("clay-eval-ns-to-hiccup", code)
 end
 
-M.ClayMakeNamespaceHtml = function()
+-- Require and evaluate clay.
+-- Evaluates current form and renders it to browser.
+M.ClayMakeFile = function()
   local source_path = vim.fn.expand("%:p")
-  local code = string.format('(scicloj.clay.v2.api.snippets/make-ns-html! "%s" {:ide :neovim})', source_path)
+  local code = string.format(
+    '(do (require \'[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/make-ns-html! "%s" {:ide :neovim}))',
+    source_path
+  )
   conj_eval("make-ns-html!", code)
 end
 
-M.ClayMakeNamespaceQuartoHtml = function()
+M.ClayMakeFileQuarto = function()
   local source_path = vim.fn.expand("%:p")
-  local code = string.format('(scicloj.clay.v2.api.snippets/make-ns-quarto-html! "%s" {:ide :neovim})', source_path)
+  local code = string.format(
+    '(do (require \'[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/make-ns-quarto-html! "%s" {:ide :neovim}))',
+    source_path
+  )
   conj_eval("make-ns-quarto-html!", code)
 end
 
-M.ClayMakeNamespaceQuartoRevealjs = function()
+M.ClayMakeFileRevealjs = function()
   local source_path = vim.fn.expand("%:p")
-  local code = string.format('(scicloj.clay.v2.api.snippets/make-ns-quarto-revealjs! "%s" {:ide :neovim})', source_path)
+  local code = string.format(
+    '(do (require \'[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/make-ns-quarto-revealjs! "%s" {:ide :neovim}))',
+    source_path
+  )
   conj_eval("make-ns-quarto-revealjs!", code)
 end
 
-M.ClayMakeFormHtml = function()
-  local form_content = extract.form({ root = true }).content
+-- Require and evaluate clay.
+-- Evaluates top level form and renders it to browser.
+M.ClayMakeCurrentForm = function()
+  local form_content = extract.form({}).content
   local source_path = vim.fn.expand("%:p")
-  local code = string.format('(scicloj.clay.v2.api.snippets/make-form-html! "%s" "%s" {:ide :neovim})', form_content, source_path)
+  local code = string.format(
+    '(do (require \'[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/make-form-html! (quote %s) "%s" {:ide :neovim}))',
+    form_content,
+    source_path
+  )
   conj_eval("make-form-html!", code)
 end
 
-M.MakeFormQuartoHtml = function()
+-- Require and evaluate clay.
+-- Evaluates top level form and renders it to browser.
+M.ClayMakeTopLevelForm = function()
   local form_content = extract.form({ root = true }).content
   local source_path = vim.fn.expand("%:p")
-  local code = string.format('(scicloj.clay.v2.api.snippets/make-form-quarto-html! "%s" "%s" {:ide :neovim})', form_content, source_path)
-  conj_eval("make-form-quarto-html!", code)
+  local code = string.format(
+    '(do (require \'[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/make-form-html! (quote %s) "%s" {:ide :neovim}))',
+    form_content,
+    source_path
+  )
+  conj_eval("make-form-html!", code)
 end
 
+M.ClayMakeCurrentFormQuarto = function()
+  local form_content = extract.form({}).content
+  local source_path = vim.fn.expand("%:p")
+  local code = string.format(
+    '(do (clojure.core/require \'[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/make-form-quarto-html! (quote %s) "%s" {:ide :neovim}))',
+    form_content,
+    source_path
+  )
+  conj_eval("make-current-form-quarto!", code)
+end
+
+
+M.ClayMakeTopLevelFormQuarto = function()
+  local form_content = extract.form({ root = true }).content
+  local source_path = vim.fn.expand("%:p")
+  local code = string.format(
+    '(do (clojure.core/require \'[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/make-form-quarto-html! (quote %s) "%s" {:ide :neovim}))',
+    form_content,
+    source_path
+  )
+  conj_eval("make-top-form-quarto!", code)
+end
+
+-- Require and evaluate clay.
+-- Opens localhost page.
 M.ClayBrowse = function()
-  local code = string.format('(scicloj.clay.v2.api.snippets/browse!)')
+  local code =
+    string.format("(do (clojure.core/require '[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/browse!))")
   conj_eval("browse!", code)
 end
 
 M.ClayWatch = function()
-  local code = string.format('(scicloj.clay.v2.api.snippets/watch! {:ide :neovim})')
+  local code = string.format(
+    "(do (clojure.core/require '[scicloj.clay.v2.snippets]) (scicloj.clay.v2.snippets/watch! {:ide :neovim}))"
+  )
   conj_eval("watch!", code)
 end
 
